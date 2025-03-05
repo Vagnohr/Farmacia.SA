43b0b252d74ba47018056f5e6f1e5146767a6a9b
CaioBattisti <cluizbattisti@gmail.com>
2025-03-05 20:09:26 -0300

    save

--- Database.py
+++ Database.py
@@ -1,1 +1,24 @@
 #Colocar no Banco de Dados
+import mysql.connector
+
+class Database:
+    def __init__(self):
+        #conecta ao banco de dados
+        self.conn = mysql.connector.connect(
+            host ="localhost",
+            user ="root", 
+            password ="",
+            Database ="CaioBattisti_db" 
+        )
+        self.cursor = self.conn.cursor()
+        self.cursor.execute('''CREATE TABLE IF NOT EXISTS 
+        usuario1(                    
+            idusuario INT AUTO_INCREMENT PRIMARY KEY               
+            nome TEXT (255),               
+            email TEXT (255),               
+            usuario TEXT (255),  
+            senha TEXT (255)                           
+        );''')
+        self.conn.commit()
+
+    print("Conectando ao Banco de Dados")
