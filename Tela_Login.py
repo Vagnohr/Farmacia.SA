e2e36d80a1ecf272bef060cb82439ae189ad6c1a
CaioBattisti <cluizbattisti@gmail.com>
2025-03-05 20:40:11 -0300

    save

--- Tela_Login.py
+++ Tela_Login.py
@@ -21,8 +21,11 @@ senhaEntry = ttk.Entry(width=30, show=".")#cria um campo de entrada para a senha
 senhaEntry.place(x=60, y=125)#posiciona o campo de entrada
 
 #criando botões
-LoginButton = ttk.Button(text="Login usuario")
-LoginButton.place(x=5,y=150)
+LoginButton1 = ttk.Button(text="Login:usuario")
+LoginButton1.place(x=5,y=150)
+
+LoginButton2 = ttk.Button(text="Login:Funcionario")
+LoginButton2.place(x=5,y=180)
 
 #função de login
 def Login():
@@ -33,29 +36,28 @@ def Login():
     db = Database()
     db.cursor.execute("""SELECT * FROM usuario1 WHERE usuario = %s""",(usuario, senha))
 
-'''#adicionar campos de usuario e senha
-usuarioLabel = Label(text="Usuario: ",font=("Century Gothic",10),bg="ORANGE",fg="White")#cria um albel para o usuario
-usuarioLabel.place(x=5, y=100)#posiciona o label no frame direito
-usuarioEntry = ttk.Entry(width=30)#cria um campo de entrada para o usuario
-usuarioEntry.place(x=65, y=100)#posiciona o campo de entrada
 
-senhaLabel = Label(text="Senha: ",font=("Century Gothic",10),bg="ORANGE",fg="White")#cria um albel para a senha
-senhaLabel.place(x=5, y=125)#posiciona o label no frame direito
-senhaEntry = ttk.Entry(width=30, show=".")#cria um campo de entrada para a senha
-senhaEntry.place(x=60, y=125)#posiciona o campo de entrada
+if LoginButton2 == True:
+    
+#adicionar campos de usuario e senha
+    usuarioLabel2 = Label(text="Usuario: ",font=("Century Gothic",10),bg="ORANGE",fg="White")#cria um albel para o usuario
+    usuarioLabel2.place(x=5, y=100)#posiciona o label no frame direito
+    usuarioEntry2 = ttk.Entry(width=30)#cria um campo de entrada para o usuario
+    usuarioEntry2.place(x=65, y=100)#posiciona o campo de entrada
 
-#criando botões
-LoginButton = ttk.Button(text="Login usuario")
-LoginButton.place(x=5,y=150)
+    senhaLabel2 = Label(text="Senha: ",font=("Century Gothic",10),bg="ORANGE",fg="White")#cria um albel para a senha
+    senhaLabel2.place(x=5, y=125)#posiciona o label no frame direito
+    senhaEntry2 = ttk.Entry(width=30, show=".")#cria um campo de entrada para a senha
+    senhaEntry2.place(x=60, y=125)#posiciona o campo de entrada
 
 #função de login
-def Login():
-    usuario = usuarioEntry.get()
-    senha = senhaEntry.get()
+    def Login():
+        usuario = usuarioEntry.get()
+        senha = senhaEntry.get()
 
 #conectar ao banco de dado
-    db = Database()
-    db.cursor.execute("""SELECT * FROM usuario1 WHERE usuario = %s""",(usuario, senha))'''
+        db = Database()
+        db.cursor.execute("""SELECT * FROM usuario1 WHERE usuario = %s""",(usuario, senha))
 
 
 jan.mainloop()
