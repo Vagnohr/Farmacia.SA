7184a26fdd633cd4320012cdcb80549fae48900b
Felipe B. Santos <felipebtst1@gmail.com>
2025-03-05 19:50:46 -0300

    Update crud_stuff_produtos.py

---
+++ crud_stuff_produtos.py
@@ -0,0 +1,44 @@
+import mysql.connector
+MYSQL_HOST='localhost'
+MYSQL_USER='root'
+MYSQL_PASSWORD=''
+MYSQL_DATABASE='prdutos'
+def get_connection():
+    return mysql.connector.connect(
+        host=MYSQL_HOST,
+        user=MYSQL_USER,
+        password=MYSQL_PASSWORD,
+        database=MYSQL_DATABASE)
+def create_product(nome,estoque,valor):
+    conn=get_connection()
+    cursor=conn.cursor()
+    query="insert produtos(nome,estoque,valor)VALUES(%s,%i,%s)"
+    cursor.execute(query,(nome,estoque,valor))
+    conn.commit()
+    cursor.close()
+    conn.close()
+def read_products():
+    conn=get_connection()
+    cursor=conn.cursor()
+    query="SELECT * FROM produtos"
+    cursor.execute(query) 
+    result=cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return result
+def update_product(nome,estoque,valor):
+    conn=get_connection()
+    cursor=conn.cursor()
+    query="UPDATE produtos SET nome=%s,estoque=%i,valor=%d"
+    cursor.execute(query,(nome,estoque,valor))
+    conn.commit()
+    cursor.close()
+    conn.close()
+def delete_product(product_id):
+    conn=get_connection()
+    cursor=conn.cursor()
+    query="DELETE FROM produtos WHERE idproduto=%s"
+    cursor.execute(query,(product_id,))
+    conn.commit()
+    cursor.close()
+    conn.close()
