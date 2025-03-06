12ff40d7bcc84c2964c909fa3c2fa80638180d0f
CaioBattisti <cluizbattisti@gmail.com>
2025-03-05 21:59:44 -0300

    save

--- Tela_Login.py
+++ Tela_Login.py
@@ -14,20 +14,11 @@ Titulo = Label(text="Login:",font=("Century Gothic",25),bg="red",fg="White")
 Titulo.place(x=1, y=50)
 
 #adicionar campos de usuario e senha
-nomeLabel = Label(text="Nome: ",font=("Century Gothic",10),bg="ORANGE",fg="White")#cria um albel para o usuario
-nomeLabel.place(x=1, y=100)#posiciona o label no frame direito
-nomeEntry = ttk.Entry(width=30)#cria um campo de entrada para o usuario
-nomeEntry.place(x=55, y=100)#posiciona o campo de entrada
 
 usuarioLabel = Label(text="Usuario: ",font=("Century Gothic",10),bg="ORANGE",fg="White")#cria um albel para o usuario
 usuarioLabel.place(x=1, y=125)#posiciona o label no frame direito
 usuarioEntry = ttk.Entry(width=30)#cria um campo de entrada para o usuario
 usuarioEntry.place(x=60, y=125)#posiciona o campo de entrada
-
-emailLabel = Label(text="email: ",font=("Century Gothic",10),bg="ORANGE",fg="White")#cria um albel para o usuario
-emailLabel.place(x=1, y=150)#posiciona o label no frame direito
-emailEntry = ttk.Entry(width=30)#cria um campo de entrada para o usuario
-emailEntry.place(x=50, y=150)#posiciona o campo de entrada
 
 senhaLabel = Label(text="senha: ",font=("Century Gothic",10),bg="ORANGE",fg="White")
 senhaLabel.place(x=1, y=175)#posiciona o label no frame direito
@@ -40,9 +31,7 @@ LoginButton1.place(x=1,y=200)
 
 #função de login
 def Login():
-    nome = nomeEntry.get()
-    usuario = usuarioEntry.get()
-    email = emailEntry.get()   
+    usuario = usuarioEntry.get()  
     senha = senhaEntry.get()
 
 #conectar ao banco de dado
