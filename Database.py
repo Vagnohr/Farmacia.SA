8feed2a03a3dc640a9bc6ff034d584557851468c
CaioBattisti <cluizbattisti@gmail.com>
2025-03-07 21:54:36 -0300

    save

--- Database.py
+++ Database.py
@@ -18,42 +18,17 @@ db = Database()
 
 def get_connection():
     return db.get_connection()
-def create_user(nome,telefone,email,usuario,senha):
+def create_user(usuario,senha):
     conn=get_connection()
     cursor=conn.cursor()
-    query="insert usuario(nome,telefone,email,usuario,senha)VALUES(%s,%s,%s,%s,%s)"
-    cursor.execute(query,(nome,telefone,email,usuario,senha))
-    conn.commit()
-    cursor.close()
-    conn.close()
-
-       
-def read_users():
-    conn=get_connection()
-    cursor=conn.cursor()
-    query="SELECT * FROM usuario"
-    cursor.execute(query)
-    result=cursor.fetchall()
-    cursor.close()
-    conn.close()
-    return result
-    
-    
-def update_user(nome,telefone,email,usuario,senha,user_id):
-    conn=get_connection()
-    cursor=conn.cursor()
-    query="UPDATE usuario SET nome=%s,telefone=%s,email=%s,usuario=%s,senha=%s WHERE idusuario=%s"
-    cursor.execute(query,(nome,telefone,email,usuario,senha,user_id))
+    query="insert usuario(usuario,senha)VALUES(%s,%s,%s,%s,%s)"
+    cursor.execute(query,(usuario,senha))
     conn.commit()
     cursor.close()
     conn.close()
 
     
-def delete_user(user_id):
-    conn=get_connection()
-    cursor=conn.cursor()
-    query="DELETE FROM usuario WHERE idusuario=%s"
-    cursor.execute(query,(user_id,))
-    conn.commit()
-    cursor.close()
-    conn.close()
+    
+
+
+    
